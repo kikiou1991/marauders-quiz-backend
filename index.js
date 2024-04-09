d2709e2fb5681cc5a18741e7c87e43315205936a
Szabó Gergő <szabogergo20011008@gmail.com>
2024-04-09 11:55:28 +0200

    d

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const cors = require('cors');
+const {Server} = require('socket.io');
+const {MongoClient} = require('mongodb');
+const jwt = require('jsonwebtoken');
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
+});
+const port = 3010;
+require('dotenv').config();
+const mongoDBUri = process.env.MONGODB_URI;
+let db;
+app.use(cors());
+app.use(express.json());
+
+app.get('/api', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.use((err, req, res, next) => {
+  console.error('Unhandled error:', err);
+  res.status(500).send('Internal Server Error');
+});
+
+const mongoClient = new MongoClient(mongoDBUri);
+const exlucdePaths = ['/api/login', '/api/register'];
+
+startServer();
+async function startServer() {
+  await mongoClient.connect();
+  console.log('Connected successfully to server');
+  db = mongoClient.db('marauders-quiz');
+  const collections = await db.listCollections().toArray();
+  console.log(collections);
+}
+server.listen(port, () => {});
